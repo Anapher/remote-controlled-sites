7907876d4bb366f7020cb8ad612bf6290cdcd0f4
Vincent (Anapher) <anapher@protonmail.com>
2022-04-17 22:30:03 +0200

    fix screen share leave room and close connection

--- src/packages/client/src/features/screen/components/ScreenShareScreenContent.tsx
+++ src/packages/client/src/features/screen/components/ScreenShareScreenContent.tsx
@@ -1,8 +1,10 @@
 import { Consumer } from 'mediasoup-client/lib/Consumer';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Socket } from 'socket.io-client';
 import connectWebRtc from '../../../app/webrtc/web-rtc-connect';
+import { WebRtcConnection } from '../../../app/webrtc/WebRtcConnection';
 import TokenRestClient from '../../../services/token-rest-client';
+import { REQUEST_LEAVE_ROOM } from '../../../shared/ws-server-messages';
 
 type Props = {
    screenName: string;
@@ -11,28 +13,40 @@ type Props = {
 };
 export default function ScreenShareScreenContent({ screenName, token, socket }: Props) {
    const videoRef = useRef<HTMLVideoElement | null>(null);
+   const [stream, setStream] = useState<MediaStream | null>(null);
 
    useEffect(() => {
+      let connection: WebRtcConnection | null = null;
+
       (async () => {
          const client = new TokenRestClient(token);
-         const connection = await connectWebRtc(screenName, client, socket);
+         connection = await connectWebRtc(screenName, client, socket);
 
-         connection.on('newConsumer', (consumer: Consumer) => {
+         connection?.on('newConsumer', (consumer: Consumer) => {
             if (consumer?.track) {
                const stream = new MediaStream();
                stream.addTrack(consumer.track);
 
-               console.log(consumer);
-
-               if (videoRef.current) {
-                  videoRef.current.srcObject = stream;
-               }
+               setStream(stream);
             }
          });
 
-         await connection.createReceiveTransport();
+         await connection?.createReceiveTransport();
       })();
+
+      return () => {
+         connection?.close();
+         connection = null;
+
+         socket.emit(REQUEST_LEAVE_ROOM);
+      };
    }, [screenName, token, socket]);
 
-   return <video ref={videoRef} autoPlay style={{ objectFit: 'contain', width: '100%', height: '100%' }} />;
+   useEffect(() => {
+      if (videoRef?.current) {
+         videoRef.current.srcObject = stream;
+      }
+   }, [stream, videoRef]);
+
+   return <video ref={videoRef} muted autoPlay style={{ objectFit: 'contain', width: '100%', height: '100%' }} />;
 }
